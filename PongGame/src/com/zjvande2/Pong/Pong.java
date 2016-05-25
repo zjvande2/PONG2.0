585b3d575c313dfb34dfb587aa8c756208db3ca5
Jason Vanderslice <Jvander352@aol.com>
2016-05-25 16:51:34 -0400

    Creation of Pong Game

---
+++ PongGame/src/com/zjvande2/Pong/Pong.java
@@ -0,0 +1,24 @@
+package com.zjvande2.Pong;
+
+/**
+ * 
+ * @author Jason Vanderslice
+ * Pong Main Class
+ * 5/25/16
+ *
+ */
+public class Pong {
+	
+	public Pong() {
+		
+	}
+	
+	public static void main (String [] args) {
+		Pong pong = new Pong();
+		pong.SayHello();
+	}
+	
+	public void SayHello() {
+		System.out.println("Say Hello");
+	}
+}
